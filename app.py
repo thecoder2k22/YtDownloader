f2b1cb6d041f5e986067144a056c5bbcfb1b7476
thecoder2k22 <144311203+thecoder2k22@users.noreply.github.com>
2024-11-18 22:57:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def get_available_formats():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run()
